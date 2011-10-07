e43d5c8afdfffe41b21921ddc98c75403b42f3c7
unknown <MikeKnoop@.(none)>
2011-10-07 12:34:22 -0500

    Adding forum fix

---
+++ www/forumfix.php
@@ -0,0 +1,34 @@
+<?php
+
+  $page_title = 'Fix';
+  $require_login = false;
+  $require_complete_account = false;
+  
+  require '../muzombies.ini.php';
+  
+  require 'module/includes.php';
+  
+  require 'module/general.php';
+
+
+$db_host = 'localhost';
+$db_user = 'web';
+$db_pwd = 'ap1be4t1fa80adfa4';
+$database = 'muzombies_prod';
+mysql_connect($db_host, $db_user, $db_pwd);
+mysql_select_db($database);
+$result = mysql_query("SELECT uid, status FROM game_xref WHERE gid='12'");
+while ($tmp = mysql_fetch_array($result)) {
+	$uid = $tmp['uid'];
+	$status = $tmp['status'];
+	if ($status == 'zombie') {
+		$GLOBALS['User']->AddForumRoleZombie($uid);
+	}
+	if ($status == 'human') {
+		$GLOBALS['User']->AddForumRoleHuman($uid);
+	}
+}
+mysql_free_result($result);
+
+?>
+done.
